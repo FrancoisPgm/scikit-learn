23618d90798574f47c7d9330be67ef45d1a2123b
François Paugam <francois.paugam@laposte.net>
2025-11-20 15:29:54 +0100

    try with a comment

--- build_tools/get_comment.py
+++ build_tools/get_comment.py
@@ -330,6 +330,14 @@ if __name__ == "__main__":
         json={"labels": ["bug", "invalid"]},
     )
     response.raise_for_status()
+    print(response)
+    response = requests.post(
+        f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments",
+        headers=get_headers(token),
+        json={"body": "coucou"},
+    )
+    response.raise_for_status()
+    print(response)
 
     if not repo or not token or not pr_number or not log_file or not run_id:
         raise ValueError(
